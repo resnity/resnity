26d734c7d63e52521dbec2c3e9849a15aad26b21
Liam Tat Tze Tey <tattzetey@gmail.com>
2023-07-10 13:45:21 +0100

    Add is nil type guard

--- libs/web-common/src/index.ts
+++ libs/web-common/src/index.ts
@@ -2,4 +2,6 @@ export { useSearchParams } from './hooks/useSearchParams';
 
 export type { SuccessHttpResponse } from './types/http-response.types';
 
+export { isNil } from './utils/guards';
+
 export { AppError } from './app-error';
